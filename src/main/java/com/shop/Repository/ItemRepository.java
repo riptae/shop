ddcd92d7a2d44107a32ae93da37d7321f6a6bb6c
riptae <riptaggart@naver.com>
2024-03-10 21:51:22 +0900

    Querydsl 사용

--- src/main/java/com/shop/Repository/ItemRepository.java
+++ src/main/java/com/shop/Repository/ItemRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.shop.entity.Item;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
     List<Item> findByItemNm(String itemNm);
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
