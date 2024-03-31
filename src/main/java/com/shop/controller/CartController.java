3b821c0e37db2f928d8a1324b63f4ef295f78555
riptae <riptaggart@naver.com>
2024-04-01 06:29:22 +0900

    Config + Controller 나머지

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -1,87 +1,110 @@
 package com.shop.controller;
-
+import com.shop.dto.CartItemDto;
+import com.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+
+import com.shop.dto.CartDetailDto;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import com.shop.dto.CartOrderDto;
 
 @Controller
 @RequiredArgsConstructor
 public class CartController {
 
-    // addCart
-    @PostMapping("/cart/addcart")
-    public @ResponseBody ResponseEntity addCart(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal) {
+    private final CartService cartService;
 
-        if (bindingResult.hasErrors()) {
+    @PostMapping(value = "/cart")
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto, BindingResult bindingResult, Principal principal){
+
+        if(bindingResult.hasErrors()){
             StringBuilder sb = new StringBuilder();
-            List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
-            for (FieldError fieldError : fieldErrorList) {
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            for (FieldError fieldError : fieldErrors) {
                 sb.append(fieldError.getDefaultMessage());
             }
 
             return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
         }
 
+        String email = principal.getName();
+        Long cartItemId;
+
         try {
-            Long cartId = cartService.addCart(cartItemDto, principal.getName());
-        } catch (Exception e) {
+            cartItemId = cartService.addCart(cartItemDto, email);
+        } catch(Exception e){
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<Long>(cartId, HttpStatus.OK);
 
-    }
-    // getCartList
-    @GetMapping("/cart")
-    public String cart(Model model, Principal principal) {
-
-        CartItemDto cartItemDto = cartService.getCart(principal.getName());
-        model.addAttribute("cart", cartItemDto);
-
-        return "/cart";
-
+        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
 
-    // UpdateCart
-    @PatchMapping("/cart/update")
-    public @ResponseBody ResponseEntity cartUpdate(Long cartItemId, int count, Principal principal) {
-        if (!cartService.validateCart(cartItemId, principal.getName()))
+    @GetMapping(value = "/cart")
+    public String orderHist(Principal principal, Model model){
+        List<CartDetailDto> cartDetailList = cartService.getCartList(principal.getName());
+        model.addAttribute("cartItems", cartDetailList);
+        return "cart/cartList";
+    }
+
+    @PatchMapping(value = "/cartItem/{cartItemId}")
+    public @ResponseBody ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId, int count, Principal principal){
+
+        if(count <= 0){
+            return new ResponseEntity<String>("최소 1개 이상 담아주세요", HttpStatus.BAD_REQUEST);
+        } else if(!cartService.validateCartItem(cartItemId, principal.getName())){
             return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
-        else if (count <= 0) {
-            return new ResponseEntity<String>("수량을 정확히 입력해주세요", HttpStatus.BAD_REQUEST);
         }
 
-        cartService.update(cartItemId, principal.getName());
+        cartService.updateCartItemCount(cartItemId, count);
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
 
-    // DeleteCart
-    @DeleteMapping("/cart/delete")
-    public @ResponseBody ResponseEntity cartDelete(Long cartItemId, Principal principal) {
-        if (!cartService.validateCart(cartItemId, principal.getName()))
+    @DeleteMapping(value = "/cartItem/{cartItemId}")
+    public @ResponseBody ResponseEntity deleteCartItem(@PathVariable("cartItemId") Long cartItemId, Principal principal){
+
+        if(!cartService.validateCartItem(cartItemId, principal.getName())){
             return new ResponseEntity<String>("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
 
-        cartService.delete(cartItemId, principal.getName());
+        cartService.deleteCartItem(cartItemId);
+
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
 
-    // Order_inCart
-    @PostMapping("cart/order")
-    public @ResponseBody ResponseEntity cartOrder(@RequestBody @Valid CartItemDto cartItemDto, Principal principal) {
+    @PostMapping(value = "/cart/orders")
+    public @ResponseBody ResponseEntity orderCartItem(@RequestBody CartOrderDto cartOrderDto, Principal principal){
 
+        List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList();
 
+        if(cartOrderDtoList == null || cartOrderDtoList.size() == 0){
+            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.FORBIDDEN);
+        }
 
-        Long orderId = cartService.orderCartItem();
+        for (CartOrderDto cartOrder : cartOrderDtoList) {
+            if(!cartService.validateCartItem(cartOrder.getCartItemId(), principal.getName())){
+                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
+            }
+        }
 
+        Long orderId = cartService.orderCartItem(cartOrderDtoList, principal.getName());
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
 
--- src/main/java/com/shop/controller/OrderController.java
+++ src/main/java/com/shop/controller/OrderController.java
@@ -1,30 +1,41 @@
 package com.shop.controller;
 
+import com.shop.dto.OrderDto;
+import com.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
-import java.awt.print.Pageable;
 import java.security.Principal;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.shop.dto.OrderHistDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.ui.Model;
 import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
 
-    // TO-do
-    @PostMapping("/order")
-    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto, BindingResult bindingResult, Principal principal) {
-        if (bindingResult.hasErrors()) {
+    private final OrderService orderService;
+
+    @PostMapping(value = "/order")
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto
+            , BindingResult bindingResult, Principal principal){
+
+        if(bindingResult.hasErrors()){
             StringBuilder sb = new StringBuilder();
             List<FieldError> fieldErrors = bindingResult.getFieldErrors();
 
@@ -39,35 +50,36 @@ public class OrderController {
         Long orderId;
 
         try {
-            orderId = orderService.order(OrderDto, email);
-        } catch (Exception e) {
+            orderId = orderService.order(orderDto, email);
+        } catch(Exception e){
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
-
     }
 
     @GetMapping(value = {"/orders", "/orders/{page}"})
-    public String orderHist(@PathVariable("page")Optional<Integer> page, Model model, Principal principal) {
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model){
 
         Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
-        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+        Page<OrderHistDto> ordersHistDtoList = orderService.getOrderList(principal.getName(), pageable);
 
-        model.addAttribute("orders", orderHistDtoList);
-        model.addAttribute("page", pageable.getNumberOfPages());
+        model.addAttribute("orders", ordersHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
         model.addAttribute("maxPage", 5);
 
-        return "/order/orderHist";
+        return "order/orderHist";
     }
 
     @PostMapping("/order/{orderId}/cancel")
-    public @ResponseBody ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId, Principal principal) {
+    public @ResponseBody ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId , Principal principal){
 
-        if (!orderService.validateOrder(orderId, principal.getName()))
-            return new ResponseEntity<String>("권한이 없습니다.", HttpStatus.FORBIDDEN);
+        if(!orderService.validateOrder(orderId, principal.getName())){
+            return new ResponseEntity<String>("주문 취소 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
 
         orderService.cancelOrder(orderId);
-        return new ResponseEntity<Integer>(orderId, HttpStatus.OK);
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
+
 }
